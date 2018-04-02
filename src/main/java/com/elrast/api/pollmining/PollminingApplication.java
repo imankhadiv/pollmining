04c72873eadf6974895a4593c3d53d5f12dc1e08
Iman <iman.khadiv@gmail.com>
2018-04-02 10:45:15 -0700

    Initial commit

---
+++ src/main/java/com/elrast/api/pollmining/PollminingApplication.java
@@ -0,0 +1,12 @@
+package com.elrast.api.pollmining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollminingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollminingApplication.class, args);
+	}
+}
